403ee2fdf8d2bf6d9c54d7bf55b59becdbc1488a
Darko Goluža <darkogoluza@gmail.com>
2024-11-12 10:25:48 +0100

    add recoil

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,5 +1,8 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
+
 [RequireComponent(typeof(Rigidbody))]
 public class Gun : MonoBehaviour
 {
@@ -7,9 +10,12 @@ public class Gun : MonoBehaviour
     [SerializeField] private int fireRate = 600; // 600 rounds per minute (RPM) 
     [SerializeField] private ParticleSystem _particleSystem;
     [SerializeField] private GameObject hitParticalEffect;
+    [SerializeField] private float recoil = 1f;
+    [SerializeField] private float recoilSpeed = 0.1f;
     
     private Rigidbody _rigidbody;
     private float timer = 0f;
+    private float recoilScale = 0f;
 
     private bool isShooting = false;
 
@@ -37,17 +43,30 @@ public class Gun : MonoBehaviour
                 AudioManager.instance.Play("ak47_shoot");
                 _particleSystem.Play();
                 
+                // Add recoil to the raycast's angle
+                Vector3 recoilOffset = transform.forward + new Vector3(
+                    Random.Range(-recoilScale * (recoil / 20), recoilScale * (recoil / 20)), // Horizontal recoil
+                    Random.Range(-recoilScale * (recoil / 10), recoilScale * (recoil / 10)), // Vertical recoil
+                    0); // No change in depth
+                
                 RaycastHit hit;
-                if (Physics.Raycast(firePoint.position, firePoint.forward, out hit))
+                if (Physics.Raycast(firePoint.position,recoilOffset, out hit))
                 {
                     Instantiate(hitParticalEffect, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
                 }
                 
                 timer = 60f / fireRate;
+                recoilScale += recoilSpeed * Time.deltaTime;
+                recoilScale = recoilScale >= 1 ? 1 : recoilScale;
             }
         }
 
         if (timer >= 0)
             timer -= Time.deltaTime;
+
+        if (recoilScale > 0 && !isShooting)
+            recoilScale -= Time.deltaTime;
+        else if(recoilScale < 0)
+            recoilScale = 0;
     }
 }
