75af5601bf717b8967f9a756d126c2c3fc530641
luka <lukaudovicic10@gmail.com>
2024-11-14 03:03:39 +0100

    added health with being able to take damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int currentHealth;
+    [SerializeField] private int maxHealth = 100;
+    
+    [SerializeField] private Sprite[] digitSprites;  
+    [SerializeField] private Image[] digitImages;    
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateNumericDisplay(currentHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0) currentHealth = 0;  
+        UpdateNumericDisplay(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void UpdateNumericDisplay(int health)
+    {
+        var hundreds = health / 100;
+        var tens = (health % 100) / 10;
+        var ones = health % 10;
+
+        if (health < 100)
+        {
+            digitImages[0].gameObject.SetActive(false);  
+        }
+        else
+        {
+            digitImages[0].gameObject.SetActive(true);   
+            digitImages[0].sprite = digitSprites[hundreds];
+        }
+
+      
+        if (health < 10)
+        {
+            digitImages[1].gameObject.SetActive(false);  
+        }
+        else
+        {
+            digitImages[1].gameObject.SetActive(true);   
+            digitImages[1].sprite = digitSprites[tens];
+        }
+        
+        digitImages[2].sprite = digitSprites[ones];
+    }
+}
